4bf5d5bfc74f20a5158971c9d01dcce53d76b85f
Ali <50011295+AliTazribine@users.noreply.github.com>
2021-12-28 23:39:31 +0100

    init

--- split_test.py
+++ split_test.py
@@ -9,8 +9,18 @@ import Utils
 import numpy as np
 
 signal=[1,2,3,4,5,6]
-liste=np.array([[1,2],[3,4],[5,6]])
+liste1=np.array([[1,2],[3,4],[5,6]])
 width=2
 sstep=2
 fs=1000
-print(Utils.split(signal,width,sstep,fs)==liste)
+liste2=Utils.split(signal,width,sstep,fs)
+ok=True
+
+for i in range(len(liste1)) :
+    for j in range(len(liste1[i])):
+        if(liste1[i][j]!=liste2[i][j]):
+            ok=False
+if ok:
+    print("La fonction divise bien la liste"+str(signal))
+else :
+    print("Il y'a une erreur dans la division")
